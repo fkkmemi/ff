3f9fad83b8a5da41d3aa65957e64c02abb1a8d58
fkkmemi <fkkmemi@gmail.com>
2019-12-10 17:37:21 +0900

    4 움직이는 화면 만들기

--- lib/main.dart
+++ lib/main.dart
@@ -9,50 +9,48 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      home: home(context)
+      home: HomeWidget()
     );
   }
-  Widget home(BuildContext context) {
+}
+
+class HomeWidget extends StatefulWidget {
+  @override
+  _HomeWidgetState createState() => _HomeWidgetState();  
+}
+class _HomeWidgetState extends State<HomeWidget> {
+  int i = 0;
+  @override
+  Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        leading: Icon(Icons.menu),
-        title: Text('hiru~!!'),
+        title: Text('hiru~'),
         actions: <Widget>[
           IconButton(
-            icon: Icon(Icons.mode_comment),
+            icon: Icon(Icons.send),
             onPressed: () {
-              print('눌렀어!');
-            },              
-          ),
-          IconButton(
-            icon: Icon(Icons.money_off),
-            onPressed: () => print('애로우!'),              
-          ),
+              setState(() => i++);
+            },
+          )
         ],
       ),
-      body: body(context)
+      body: XXX(i: i)
     );
   }
-  Widget body(BuildContext context) {
-    return Container(
-      padding: EdgeInsets.all(20),
-      // decoration: BoxDecoration(
-      //   color: Colors.red,
-      // ),
-      child: testList(context) // cols() // listViewTest2(context)
-    );
-  }  
+}
 
-  Widget testList (BuildContext context) {
-    List<String> items = List.generate(100, (i) => '리스트 테스트 $i');
-    return ListView.builder(
-      itemCount: items.length,
-      itemBuilder: (context, i) {
-        return ListTile(
-          title: Text(items[i]),
-          subtitle: Text('this is test $i ㅎㅎㅎㅎㅎㅎ')
-        );
-      },
+class XXX extends StatelessWidget {
+  XXX({Key key, this.i: 0}) : super(key: key);
+  final int i;
+  @override
+  Widget build(BuildContext context) {
+    return Center(
+      child: _change(i)
     );
+  }
+  Widget _change (i) {
+    if (i == 0) return CircularProgressIndicator();
+    else if (i == 1) return Icon(Icons.settings);
+    else return Text('yyy: $i');
   }
 }
